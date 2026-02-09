4db1d114c77ae79243afc47832600bfacd1447a0
tenseurevyeux <tenseurevyeux@gmail.com>
2026-02-09 13:28:59 +0500

    perf(core): parallelize mandelbrot calculation using rayon
    The program execution speed test can be found in the time.md file, in
    the mandelbrot-v1.0.0 line.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use image::GrayImage;
 use num::Complex;
+use rayon::{
+    iter::{IndexedParallelIterator, ParallelIterator},
+    slice::ParallelSliceMut,
+};
 use std::error::Error;
 
 fn calc_mandelbrot(
@@ -10,33 +14,26 @@ fn calc_mandelbrot(
     y_max: f64,
     width: usize,
     height: usize,
-) -> Vec<Vec<usize>> {
-    let mut rows: Vec<Vec<usize>> = Vec::with_capacity(height);
+) -> Vec<usize> {
+    let mut buf: Vec<usize> = vec![0; width * height];
 
-    for img_y in 0..height {
-        let mut row: Vec<usize> = Vec::with_capacity(width);
-        for img_x in 0..width {
-            let x_percent = img_x as f64 / width as f64;
-            let y_percent = img_y as f64 / height as f64;
-
-            let cx = x_min + (x_max - x_min) * x_percent;
-            let cy = y_min + (y_max - y_min) * y_percent;
-
-            let point = mandelbrot_rander_point(cx, cy, max_iters);
-            row.push(point);
+    buf.par_chunks_mut(width).enumerate().for_each(|(y, row)| {
+        let cy = y_min + (y_max - y_min) * (y as f64 / height as f64);
+        for (x, pixel) in row.iter_mut().enumerate() {
+            let cx = x_min + (x_max - x_min) * x as f64 / width as f64;
+            *pixel = mandelbrot_at_point(cx, cy, max_iters);
         }
-        rows.push(row);
-    }
+    });
 
-    rows
+    buf
 }
 
-fn mandelbrot_rander_point(cx: f64, cy: f64, max_iters: usize) -> usize {
+fn mandelbrot_at_point(cx: f64, cy: f64, max_iters: usize) -> usize {
     let mut z = Complex::new(0.0, 0.0);
     let c = Complex::new(cx, cy);
 
     for i in 0..=max_iters {
-        if z.norm() > 2.0 {
+        if z.norm_sqr() > 4.0 {
             return i;
         }
         z = z * z + c;
@@ -46,27 +43,21 @@ fn mandelbrot_rander_point(cx: f64, cy: f64, max_iters: usize) -> usize {
 }
 
 fn draw_mandelbrot(
-    escaped: Vec<Vec<usize>>,
+    escaped: Vec<usize>,
     width: u32,
     height: u32,
     max_iters: usize,
 ) -> Result<(), Box<dyn Error>> {
     let raw_img: Vec<u8> = escaped
         .iter()
-        .map(|inner| {
-            inner
-                .iter()
-                .map(|&x| {
-                    if x == max_iters {
-                        255u8
-                    } else {
-                        ((x as f32 / max_iters as f32) * 255.0) as u8
-                    }
-                })
-                .collect::<Vec<u8>>()
+        .map(|&x| {
+            if x == max_iters {
+                255u8
+            } else {
+                ((x as f32 / max_iters as f32) * 255.0) as u8
+            }
         })
-        .flatten()
-        .collect();
+        .collect::<Vec<u8>>();
 
     let img = match GrayImage::from_raw(width, height, raw_img) {
         Some(im) => im,
@@ -84,9 +75,9 @@ fn draw_mandelbrot(
 }
 
 fn main() {
-    let escaped = calc_mandelbrot(1000, -2.0, 1.0, -1.0, 1.0, 3840, 2160);
+    let escaped = calc_mandelbrot(1000, -2.0, 1.0, -0.84375, 0.84375, 3840, 2160);
 
-    let _ = match draw_mandelbrot(escaped, 1920, 1080, 1000) {
+    let _ = match draw_mandelbrot(escaped, 3840, 2160, 1000) {
         Ok(_) => println!("Successed save image as \"image.png\""),
         Err(e) => println!("Handled error: {}", e),
     };
--- test-info/time.md
+++ test-info/time.md
@@ -1,3 +1,4 @@
-| Command | Mean [s] | Min [s] | Max [s] | Relative |
+| Version | Mean [s] | Min [s] | Max [s] | Relative |
 |:---|---:|---:|---:|---:|
 | `mandelbrot_beta(Initial Commit)` | 8.532 ± 0.020 | 8.491 | 8.555 | 1.00 |
+| `mandelbrot-v1.0.0` | 594.8 ± 14.2 | 572.1 | 613.2 | 1.00 |
